20d6eb0bb93084254dae99cf2ecf68847d09c6ca
tomo-ki <69376551+tomo-ki@users.noreply.github.com>
2023-07-11 00:25:42 +0900

    nested-layout

--- rsc-supabase/app/page.tsx
+++ rsc-supabase/app/page.tsx
@@ -1,11 +1,17 @@
+import { Suspense } from 'react'
 import NotesList from './components/notes-list'
 import TimerCounter from './components/timer-counter'
+import Spinner from './components/spinner'
+import RefreshBtn from './components/refresh-btn'
 
 export default function Home() {
   return (
     <main className="m-10 text-center">
-      <NotesList />
+      <Suspense fallback={<Spinner color="border-green-500" />}>
+        <NotesList />
+      </Suspense>
       <TimerCounter />
+      <RefreshBtn />
     </main>
   )
 }
